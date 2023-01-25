20f559016c8f41a73f095e57c2366c3f964c27ab
engineer213134 <66885586+engineer213134@users.noreply.github.com>
2023-01-25 07:45:01 -0800

    ApplicationLoadbalancer
    This is a application load balancer for are EC2 instances

---
+++ LoadBalancers/ApplicationLoadBalancers.tf
@@ -0,0 +1,4 @@
+#Treating a load balancer this will allow you to deploy traffic amoung servers
+#There are 3 types of load balancers ALB,NLB,CLB
+#This will be a ALB(Application load balancer)
+#ALB consists of several parts: Listner,Listner Rule,Target groups,
